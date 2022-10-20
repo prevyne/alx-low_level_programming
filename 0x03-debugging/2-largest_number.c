bd72880c9613a40757fac244d0fa896c9c6caf9a
= <=>
2022-10-20 15:49:22 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,26 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b)
 	{
-		 largest = a;
+		if (b > c)
+		{
+			largest = a;
+		}
+		else if (c > a)
+		{
+			largest = c;
+		}
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (c >b && b > a)
-	{
-		largest = c;
-	}
+		else
+		{
+			if (b > c)
+			{
+				largest = b;
+			}
+			else
+				largest = c;
+		}
 
-	return (largest);
+		return (0);
 }
