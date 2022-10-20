b16642575bb72af2668c5a207f6aae1ccdb3f23e
= <=>
2022-10-20 13:49:34 -0700

    positive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - tests if a number is positive or negative.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
