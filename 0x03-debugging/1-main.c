f9201f1f768a8fe2c8f50194d7e6672b80b828a8
= <=>
2022-10-20 15:14:32 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,11 @@ int main(void)
 	printf("Infinite loop icoming :(\n");
 
 	i = 0;
-
-	/*while (i < 0)
-	{
-		putchar(i);
-	} */
-
+	/**while (i < 0)
+	*{
+	*	putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
