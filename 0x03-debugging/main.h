25e414e0838545f77bca2e0ff57754b40c4b5b73
= <=>
2022-10-20 14:44:33 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN.H
+#define MAIN.H
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int h);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-int _putchar(char c);
-void print_to_98(int n);
-int postitive_or_negative(int i)
+int positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
