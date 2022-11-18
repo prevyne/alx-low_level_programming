2e739b4e213947c2b70582a97640756edc98a964
= <=>
2022-11-17 17:13:13 -0800

    mandatory

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: pointer to the integer
+ * @action: pointer to the function
+ * @size: is the size of the array
+ * Return: 0
+**/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+size_t i;
+
+if (array == NULL || action == NULL)
+{
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ *p_add - calculates the sum of two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of int 1 and 2
+**/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - calculates the difference of numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: difference between a and b
+**/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the product of two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: product
+**/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divides the first number by the second
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+**/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Calculates the modulus
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+**/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
