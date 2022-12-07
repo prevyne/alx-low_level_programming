01b854e42fbf971570939dadbc7a0925c146413f
prevyne <prevyne0ketch6@gmail.com>
2022-12-07 04:08:05 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -2,7 +2,7 @@
 /**
  * binary_to_uint - Entry Point
  * @b: const char
- * Return: 0
+ * return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -11,14 +11,12 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-
 	while (b[i + 1])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
-
 	while (i >= 0)
 	{
 		res += ((b[i] - '0') * base);
@@ -26,7 +24,5 @@ unsigned int binary_to_uint(const char *b)
 		i--;
 	}
 
-
 	return (res);
-
 }
